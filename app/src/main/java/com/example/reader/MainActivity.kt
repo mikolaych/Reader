93d1aeded5593a740d92cdbc724b7f073d8a01bd
mikolaych <mikolaych@mail.ru>
2023-07-01 00:35:24 +0200

    010723

--- app/src/main/java/com/example/reader/MainActivity.kt
+++ app/src/main/java/com/example/reader/MainActivity.kt
@@ -2,44 +2,107 @@ package com.example.reader
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.os.SystemClock
 import android.view.View
+import android.widget.Chronometer
+import android.widget.Toast
 import com.example.reader.databinding.ActivityMainBinding
 import kotlinx.coroutines.NonCancellable.start
+import kotlin.system.measureTimeMillis
+
+var mainList = listOf<String>()
+var endArray = emptyArray<String>()
+var tempArray = emptyArray<String>()
+var wordNum = 0
+var stTime : Long = 0
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+
+    private var timer: CountDownTimer? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
-        startBtn()
+        inputText()
+        ready()
+        stop()
+
+
+
+
     }
 
-    private fun startBtn() {
-        binding.btnStart.setOnClickListener {
+
+
+    private fun inputText() {
+        binding.btnInput.setOnClickListener {
             if (binding.editText.text.isNullOrBlank()) {
                 binding.info.visibility = View.VISIBLE
                 binding.info.text = "Введите текст!"
-            } else if (binding.inputTimer.text.isNullOrBlank()) {
-                binding.info.visibility = View.VISIBLE
-                binding.info.text = "Введите таймер!"
             } else {
-                visibilityOn()
+                binding.btnReady.visibility = View.VISIBLE
+                binding.btnInput.visibility = View.INVISIBLE
+                binding.editText.visibility = View.INVISIBLE
+                binding.timer.visibility = View.VISIBLE
+                var inText = binding.editText.text.toString()
+                val delimeter = " "
+                mainList = inText.split(delimeter)
+
             }
         }
+    }
+
+    private fun ready() {
+        binding.btnReady.setOnClickListener {
+
+            if (wordNum< mainList.size){
+            timeToReady()
+                visibilityOn()
+            } else end()
+        }
+    }
+
+
+    private fun timeToReady() {
+        timer?.cancel()
+        timer = object : CountDownTimer(3000,1000){
+            override fun onTick(p0: Long) {
+
+                binding.timer.text = (p0/1000).toString()
+
+            }
+            override fun onFinish() {
+                    binding.viewText.text = mainList[wordNum]
+                    wordNum++
+                binding.btnReady.visibility = View.INVISIBLE
+                binding.btnStop.visibility = View.VISIBLE
+
+
+            }
+        }.start()
+    }
+
+
+
+    private fun stop() {
 
     }
 
+    private fun end() {
+
+    }
+
+
     private fun visibilityOn() {
-        binding.headInputTime.visibility = View.INVISIBLE
-        binding.inputTimer.visibility = View.INVISIBLE
         binding.editText.visibility = View.INVISIBLE
-        binding.btnStart.visibility = View.INVISIBLE
+        binding.btnInput.visibility = View.INVISIBLE
         binding.viewText.visibility = View.VISIBLE
-        binding.btnExam.visibility = View.VISIBLE
         binding.shText.visibility = View.VISIBLE
-        binding.timer.visibility = View.VISIBLE
+
         binding.headFalse.visibility = View.VISIBLE
         binding.headTrue.visibility = View.VISIBLE
         binding.trueWin.visibility = View.VISIBLE
@@ -51,12 +114,9 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun visibilityOff() {
-        binding.headInputTime.visibility = View.VISIBLE
-        binding.inputTimer.visibility = View.VISIBLE
+        binding.btnInput.visibility = View.VISIBLE
         binding.editText.visibility = View.VISIBLE
-        binding.btnStart.visibility = View.VISIBLE
         binding.viewText.visibility = View.INVISIBLE
-        binding.btnExam.visibility = View.INVISIBLE
         binding.shText.visibility = View.INVISIBLE
         binding.timer.visibility = View.INVISIBLE
         binding.headFalse.visibility = View.INVISIBLE
